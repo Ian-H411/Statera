baa196ea6afe61f66a6774ec5e8eb72392e10bcc
Ian Hall <h411.ian@gmail.com>
2023-10-29 09:47:34 -0600

    Initial Project commit
    creates project

---
+++ Statera/Statera/StateraApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateraApp.swift
+//  Statera
+//
+//  Created by Ian Hall on 10/29/23.
+//
+
+import SwiftUI
+
+@main
+struct StateraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
