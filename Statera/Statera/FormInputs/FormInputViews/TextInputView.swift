3e702cf1c52bd3f7f40922893509f91db1a9bb35
Ian Hall <h411.ian@gmail.com>
2023-11-04 12:04:49 -0600

    begin text input view

--- Statera/Statera/FormInputs/FormInputViews/TextInputView.swift
+++ Statera/Statera/FormInputs/FormInputViews/TextInputView.swift
@@ -5,4 +5,35 @@
 //  Created by Ian Hall on 11/4/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct TextInputView: View {
+    @Binding var text: String
+    var minCharacters: Int
+    var maxCharacters: Int
+    var allowedCharacterSet: CharacterSet
+    
+    var body: some View {
+        TextField("Enter Text", text: $text)
+            .onAppear(perform: {
+                if text.count < minCharacters {
+                    text = String(repeating: " ", count: minCharacters)
+                } else if text.count > maxCharacters {
+                    text = String(text.prefix(maxCharacters))
+                }
+            })
+            .onChange(of: text, initial: true) { _, newValue in
+                let filteredText = newValue.filter { allowedCharacterSet.contains($0) }
+                if filteredText.count > maxCharacters {
+                    text = String(filteredText.prefix(maxCharacters))
+                } else {
+                    text = filteredText
+                }
+            }
+    }
+}
+
+
+#Preview {
+    TextInputView(text: "hi", minCharacters: 2, maxCharacters: 8, allowedCharacterSet: .alphanumerics)
+}
