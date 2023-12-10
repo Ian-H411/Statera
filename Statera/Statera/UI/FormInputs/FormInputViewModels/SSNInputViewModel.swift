8215c27683a60b9cb4baa15e54907b4d1408f6c6
Ian Hall <h411.ian@gmail.com>
2023-12-10 05:09:11 -0700

    updated SSN field

--- Statera/Statera/UI/FormInputs/FormInputViewModels/SSNInputViewModel.swift
+++ Statera/Statera/UI/FormInputs/FormInputViewModels/SSNInputViewModel.swift
@@ -9,8 +9,30 @@ import Foundation
 
 class SSNInputViewModel: FormInputViewModel {
     
-    override init(labelText: String, preFill: String,  isRequired: Bool = true) {
+    private let ssnRegex = "^(?!666|000|9\\d{2})\\d{3}-(?!00)\\d{2}-(?!0{4})\\d{4}$"
+    
+    var allowedCharacterSet: CharacterSet
+    var minCharacters: Int
+    var maxCharacters: Int
+    
+    override init(labelText: String, preFill: String, isRequired: Bool = true) {
+        self.allowedCharacterSet = CharacterSet.decimalDigits
+        self.minCharacters = 9
+        self.maxCharacters = 9
         super.init(labelText: labelText, preFill: preFill, isRequired: isRequired)
         self.questionType = .ssn
+    }
+
+    func updateText(_ newValue: String) {
+        let filteredText = newValue.filter { char in
+            guard let unicodeScaler = char.unicodeScalars.first else { return false }
+            return allowedCharacterSet.contains(unicodeScaler)
+        }
+        self.userInput = String(filteredText.prefix(maxCharacters))
+    }
+
+    func isValidSSN() -> Bool {
+        let ssnTest = NSPredicate(format:"SELF MATCHES %@", ssnRegex)
+        return ssnTest.evaluate(with: userInput)
     }
 }
--- Statera/Statera/UI/FormInputs/FormInputViews/SSNInputView.swift
+++ Statera/Statera/UI/FormInputs/FormInputViews/SSNInputView.swift
@@ -5,4 +5,31 @@
 //  Created by Ian Hall on 11/4/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct SSNInputView: View {
+    @ObservedObject var viewModel: SSNInputViewModel
+
+    var body: some View {
+        VStack {
+            TextField(viewModel.labelText, text: $viewModel.userInput)
+                .keyboardType(.numberPad)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+                .onChange(of: viewModel.userInput, initial: true) { _, newValue  in
+                    viewModel.updateText(newValue)
+                }
+
+            if !viewModel.isValidSSN() {
+                Text("Invalid SSN")
+                    .foregroundColor(.red)
+                    .padding(.top, 4)
+            }
+        }
+    }
+}
+
+#Preview {
+    let viewModel = SSNInputViewModel(labelText: "whats your SSN", preFill: "")
+    SSNInputView(viewModel: viewModel)
+}
