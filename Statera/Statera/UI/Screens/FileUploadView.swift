05de41b44fdd833d5d24b51dbc7c87e46dc25341
Ian Hall <h411.ian@gmail.com>
2023-12-31 15:00:29 -0700

    file upload view and vm
    base boiler plate

---
+++ Statera/Statera/UI/Screens/FileUploadView.swift
@@ -0,0 +1,23 @@
+//
+//  FileUploadView.swift
+//  Statera
+//
+//  Created by Ian Hall on 12/31/23.
+//
+
+import SwiftUI
+
+struct FileUploadView: View {
+    
+    @ObservedObject var fileUploadViewModel: FileUploadViewModel = FileUploadViewModel()
+    
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    FileUploadView()
+}
---
+++ Statera/Statera/UI/ViewModels/FileUploadViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FileUploadViewModel.swift
+//  Statera
+//
+//  Created by Ian Hall on 12/31/23.
+//
+
+import Foundation
+
+class FileUploadViewModel: ObservableObject {
+    
+}
