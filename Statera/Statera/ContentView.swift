968937b7d585e454dabac5234dce21f4fc5a1d82
Ian Hall <h411.ian@gmail.com>
2024-02-04 09:56:45 -0700

    reorganization of flow director on contentview

--- Statera/Statera/ContentView.swift
+++ Statera/Statera/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isLoggedIn = false
+    @State private var isFirstLogin = false
     @StateObject private var errorViewModel = ErrorViewModel()
     @State private var displayMenu = false
     @State var path = NavigationPath()
+    
     var body: some View {
         NavigationStack(path: $path) {
             ZStack{
@@ -24,47 +26,61 @@ struct ContentView: View {
                         ErrorBannerView(errorViewModel: errorViewModel)
                             .frame(width: 330, height: 120)
                     }
-                    if isLoggedIn {
-                        FormScreenView(errorViewModel: errorViewModel)
-                    } else {
-                        LoginView(isLoggedIn: $isLoggedIn, errorViewModel: errorViewModel)
-                    }
-                    
+                    flowDirector()
                 }
-                if displayMenu {
-                    Color.black.opacity(0.3)
-                        .edgesIgnoringSafeArea(.all)
-                        .onTapGesture {
-                            displayMenu = false
-                        }
-                    VStack {
-                        HStack {
-                            Spacer()
-                                .frame(width: .infinity)
-                            FloatingMenu(isMenuDisplayed: $displayMenu, isLoggedIn: $isLoggedIn)
-                        }
-                        
-                        Spacer()
-                    }
-                    .onTapGesture {
-                        displayMenu = false
-                    }
-                }
+                popUpMenu()
             }
             .toolbar(content: {
                 ToolbarItemGroup(placement: .confirmationAction) {
-                    Button(action: {
-                        displayMenu.toggle()
-                    }, label: {
-                        Image(systemName: "line.horizontal.3")
-                            .imageScale(.large)
-                            .padding()
-                    })
+                    hamburgerMenuContent()
                 }
             })
         }
         .navigationTitle("Login")
-
+    }
+    
+    @ViewBuilder
+    func flowDirector() -> some View {
+        if isLoggedIn && isFirstLogin {
+            FormScreenView(errorViewModel: errorViewModel)
+        } else if isLoggedIn && isFirstLogin {
+            HomeScreen(errorViewModel: errorViewModel)
+        } else {
+            LoginView(isLoggedIn: $isLoggedIn, errorViewModel: errorViewModel)
+        }
+    }
+    
+    @ViewBuilder
+    func popUpMenu() -> some View {
+        if displayMenu {
+            Color.black.opacity(0.3)
+                .edgesIgnoringSafeArea(.all)
+                .onTapGesture {
+                    displayMenu = false
+                }
+            VStack {
+                HStack {
+                    Spacer()
+                        .frame(width: .infinity)
+                    FloatingMenu(isMenuDisplayed: $displayMenu, isLoggedIn: $isLoggedIn)
+                }
+                Spacer()
+            }
+            .onTapGesture {
+                displayMenu = false
+            }
+        }
+    }
+    
+    @ViewBuilder
+    func hamburgerMenuContent() -> some View {
+        Button(action: {
+            displayMenu.toggle()
+        }, label: {
+            Image(systemName: "line.horizontal.3")
+                .imageScale(.large)
+                .padding()
+        })
     }
 }
 
